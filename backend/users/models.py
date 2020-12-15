a9e4bd759b890b450cac5f9f686603650d317a00
Your Name <ghasemi>
2020-12-15 11:07:33 +0330

    Fix Adress and telephone

--- backend/users/models.py
+++ backend/users/models.py
@@ -4,4 +4,4 @@ from django.db import models
 class Detail(models.Model):
     number = models.CharField(max_length=15, primary_key=True)
     description = models.CharField(max_length=300)
-    inpfile = models.FileField(upload_to='uploads/')
+    inpfile = models.FileField(upload_to='upload
